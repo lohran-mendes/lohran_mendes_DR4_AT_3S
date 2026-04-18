14de4da07dcafeeee6a8b37397b6cca983db6fa9
Lohran Mendes <lohranfm@gmail.com>
2026-04-18 13:53:36 -0300

    feat(question 14): enhance layout and styling of VehicleDetails component

--- src/pages/vehicle-details/VehicleDetails.tsx
+++ src/pages/vehicle-details/VehicleDetails.tsx
@@ -1,22 +1,27 @@
 import type { Vehicle } from "../../types/vehicle.types";
 import "./styles.css";
 
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 type VehicleDetailsProps = {
   vehicle: Vehicle[];
 };
 
 export function VehicleDetails({ vehicle }: VehicleDetailsProps) {
+  const navigate = useNavigate();
+
   const { id } = useParams<{ id: string }>();
-  const selectedVehicle = vehicle.find( v => v.licensePlate === id);
+  const selectedVehicle = vehicle.find((v) => v.licensePlate === id);
 
   return (
     <section className="vehicle-details-page">
-      <h1>
-        Visualizando dados do veículo <br />
-        placa/ID: <span className="vehicle-plate">{id}</span>
-      </h1>
+      <div className="header-title">
+        <h1>
+          Visualizando dados do veículo <br />
+          placa/ID: <span className="vehicle-plate">{id}</span>
+        </h1>
+        <button onClick={() => navigate(-1)}>Voltar para a lista</button>
+      </div>
       <div className="vehicle-card">
         <div className="vehicle-info">
           <span className="vehicle-label">Marca</span>
